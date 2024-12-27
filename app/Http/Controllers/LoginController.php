f29327dd27fc290b36f7cf228b34d66ac017dc47
Atherizz <saveroathallah86@gmail.com>
2024-12-27 22:29:18 +0700

    login auth

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -12,5 +14,23 @@ class LoginController extends Controller
             'title' => 'Login'
         ]
     );
+    }
+
+    public function authenticate(Request $request) {
+
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'login' => 'login failed',
+        ]);
     }
 }
